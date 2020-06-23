7a50d327f39594d456d1ffde85f70495ce51958e
whktedv <65950901+whktedv@users.noreply.github.com>
2020-06-23 16:26:48 +0200

    Update ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -14,7 +14,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '1.0.95',
     'constraints' => [
         'depends' => [
-            'typo3' => '6.2.0-9.5.99',
+            'typo3' => '8.7.0-9.5.99',
         ],
         'conflicts' => [],
         'suggests' => [],
