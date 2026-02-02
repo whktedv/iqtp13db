00738c4f6622ced014ed8286f13cb29a8216e602
Uli Dohmen <edv@whkt.de>
2026-02-02 16:41:46 +0100

    Bugfix 2 02.02.2026

--- Classes/Controller/BackendController.php
+++ Classes/Controller/BackendController.php
@@ -555,7 +555,7 @@ class BackendController extends \TYPO3\CMS\Extbase\Mvc\Controller\ActionControll
                         
         $mail4externstandardmailtext = \TYPO3\CMS\Extbase\Utility\LocalizationUtility::translate('mailtextedit', 'Iqtp13db');
         
-        $gruppenberatungen = array();
+        $gruppenberatungenarr = array();
         $gruppenberatungen = $this->gruppenberatungRepository->findAvailable($this->niqbid);
         foreach($gruppenberatungen as $gb) {
             $gruppenberatungenarr[$gb->getUid()] = $gb->getTitel();
